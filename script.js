63efe88e7568e34018087c36d73b89dc020bb9af
Adrian Hatfield <99419677+adrianwhatfield@users.noreply.github.com>
2023-01-14 14:31:31 +0000

    Create changeSize()

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
-let gridSize = 16;
-
-let squareSize = 500 / gridSize;
-let squareSizeString = squareSize.toString() + "px";
-
-let containerSize = 500 + (gridSize * 2);
-let containerSizeString = containerSize.toString() + "px";
+let gridSize = "16";
 
 const container = document.getElementById("container");
+const changeSizeButton = document.getElementById("changeSize");
 
-let auto = "auto ".repeat(gridSize);
+changeSizeButton.addEventListener("click", changeSize);
 
 container.style.gridTemplateColumns = auto;
 container.style.gridTemplateRows = auto;
 container.style.width = containerSizeString;
 container.style.height = containerSizeString;
 
+setGrid();
+
+function changeSize() {
+    clearGrid();
+    gridSize = prompt("Grid size?", "16");
+    setGrid();
+}
+
+function clearGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
 function setGrid(gridArea) {
+    gridSize = parseInt(gridSize);
     gridArea = gridSize * gridSize;
+
+    if (gridSize > 100) {
+        gridSize = 100;
+    };
+
+    squareSize = 700 / gridSize;
+    squareSizeString = squareSize.toString() + "px";
 
     for (let i = 0; i < gridArea; i++) {
         const square = document.createElement("div");
         square.setAttribute("id", i);
-        square.classList.toggle("squareItem");
 
         square.style.width = squareSizeString;
         square.style.height = squareSizeString;
@@ -33,9 +49,14 @@ function setGrid(gridArea) {
             document.getElementById(i).style.backgroundColor = "black";
         });
     };
+
+    containerSize = 700 + (gridSize * 2);
+    containerSizeString = containerSize.toString() + "px";
+
+    auto = "auto ".repeat(gridSize);
+
+    container.style.gridTemplateColumns = auto;
+    container.style.gridTemplateRows = auto;
+    container.style.width = containerSizeString;
+    container.style.height = containerSizeString;
 };
-
-setGrid();
-
-// Need to get all squares, listen for hover (mouseover/out), 
-// then toggle blackBg for that one square
